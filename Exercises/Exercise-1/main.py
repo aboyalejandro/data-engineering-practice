9009931b348248884c0722e90a11b83fdb26776d
aboyalejandro <aboyalejandro@gmail.com>
2023-11-07 18:52:33 +0100

    added try-except and logger

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -7,6 +7,10 @@ import os
 import zipfile
 from io import BytesIO
 from zipfile import ZipFile
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -24,26 +28,31 @@ def main():
 
     if not os.path.exists(directory):
         os.mkdir(directory)
-        print(f"Folder '{directory}' has been created! ")
+        logger.info(f"Folder '{directory}' has been created!")
     else:
-        print(f"Folder '{directory}' already exists. ")
+        logger.info(f"Folder '{directory}' already exists.")
 
     for uri in download_uris:
-      file_name = os.path.join(directory, os.path.basename(uri))
-      response = requests.get(uri, stream=True)
-      if response.status_code == 200:
-        print(f"{uri} is OK, saving to /{directory} directory.")
-        with open(file_name, 'wb') as file:
-            for chunk in response.iter_content(chunk_size=8192):
-              file.write(chunk)
+      try:
+        file_name = os.path.join(directory, os.path.basename(uri))
+        response = requests.get(uri, stream=True)
+        if response.status_code == 200:
+          logger.info(f"{uri} is OK, saving to /{directory} directory.")
+          with open(file_name, 'wb') as file:
+              for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
 
-        with zipfile.ZipFile(file_name, 'r') as zip_ref:
-            zip_ref.extractall(directory)
+          with zipfile.ZipFile(file_name, 'r') as zip_ref:
+              zip_ref.extractall(directory)
 
-        print(f"CSV files extracted from {file_name}.")
+          logger.info(f"CSV files extracted from {file_name}.")
 
-        os.remove(file_name)
-        print(f"{file_name} deleted.")
+          os.remove(file_name)
+
+          logger.info(f"{file_name} deleted.")
         
-      else:
-        print(f"{uri} is broken, can't be unzipped.")
+        else:
+          logger.error(f"{uri} is broken, can't be unzipped.")
+        
+      except Exception as err:
+        print(err)
